341143d0218a68744d0e6d85f503ca5e028d7869
Karl Marti Toots <karl.marti.toots@estcube.eu>
2019-12-02 23:28:29 +0200

    Added all current configuration settings under GUI categorized folders.

--- src/main.js
+++ src/main.js
@@ -28,9 +28,9 @@ function onLoad() {
 
   scene = new THREE.Scene();
 
-  setCustomConf();
-
   var generator = new Generator(confMap);
+
+  setupGui(generator);
 
   core = generator.generate(seed);
 
@@ -42,52 +42,99 @@ function onLoad() {
   // Initalize keyboard and mouse controls
   initializeControls(camController);
 
-  // DAT.GUI Related Stuff
-  var guiObj = {
-    seed : globalThis.seed,
-    Generate : function() {
-      scene.remove(core.root);
-      core = generator.generate(this.seed);
-      scene.add(core.root);
-    },
-    StarScaleX : core.mesh.children[0].scale.x
-  };
-  console.log(guiObj);
-  var gui = new dat.GUI();
-  gui.add(guiObj, 'seed');
-  gui.add(guiObj, 'Generate');
-
-  var starFolder = gui.addFolder('Star');
-  starFolder.add(core.mesh.children[0].material, 'wireframe').listen();
-  starFolder.add(core.mesh.children[0].scale, 'x', 1, 10).listen();
-  starFolder.open();
-
   draw();
 }
 
 var counter = 0;
 
-function setCustomConf() {
+function setupGui(generator) {
+  var gui = new dat.GUI();
+
+  var guiObj = createGuiObject(generator);
+
+  gui.add(guiObj, 'seed');
+  gui.add(guiObj, 'Generate');
+  gui.add(guiObj, 'celBodyRotationsPerUnit', 0, 100);
+  gui.add(guiObj, 'minRevolutionsPerUnit', 0, 100);
+  gui.add(guiObj, 'maxRevolutionsPerUnit', 0, 100);
+  gui.add(guiObj, 'minTilt', -Math.PI / 2, Math.PI / 2);
+  gui.add(guiObj, 'maxTilt', -Math.PI / 2, Math.PI / 2);
+  var starConfFolder = gui.addFolder('Star Configuration');
+  starConfFolder.add(guiObj, 'starSize', 1, 100);
+
+  var planetConfFolder = gui.addFolder('Planet Configuration');
+  planetConfFolder.add(guiObj, 'planetMinSize', 1, 100);
+  planetConfFolder.add(guiObj, 'planetMaxSize', 1, 100);
+
+  var moonConfFolder = gui.addFolder('Moon Configuration');
+  moonConfFolder.add(guiObj, 'minMoonRevolutionsPerUnit', 0, 100);
+  moonConfFolder.add(guiObj, 'maxMoonRevolutionsPerUnit', 0, 100);
+  moonConfFolder.add(guiObj, 'minMoonAmount', 0, 10);
+  moonConfFolder.add(guiObj, 'maxMoonAmount', 0, 10);
+  moonConfFolder.add(guiObj, 'moonMinTilt', -Math.PI / 2, Math.PI / 2);
+  moonConfFolder.add(guiObj, 'moonMaxTilt', -Math.PI / 2, Math.PI / 2);
+
+  var orbitsConfFolder = gui.addFolder('Orbits Configuration');
+  orbitsConfFolder.add(guiObj, 'minDistanceBetweenOrbits', 10, 100);
+  orbitsConfFolder.add(guiObj, 'visibleOrbits');
+  orbitsConfFolder.add(guiObj, 'ellipticalOrbit');
+  orbitsConfFolder.add(guiObj, 'minOrbitTiltX', - Math.PI / 2, Math.PI / 2);
+  orbitsConfFolder.add(guiObj, 'maxOrbitTiltX', - Math.PI / 2, Math.PI / 2);
+  orbitsConfFolder.add(guiObj, 'minOrbitTiltZ', - Math.PI / 2, Math.PI / 2);
+  orbitsConfFolder.add(guiObj, 'maxOrbitTiltZ', - Math.PI / 2, Math.PI / 2);
+  orbitsConfFolder.add(guiObj, 'minEllipseX', 0.3, 3.0);
+  orbitsConfFolder.add(guiObj, 'maxEllipseX', 0.3, 3.0);
+  orbitsConfFolder.add(guiObj, 'minEllipseZ', 0.3, 3.0);
+  orbitsConfFolder.add(guiObj, 'maxEllipseZ', 0.3, 3.0);
+  orbitsConfFolder.add(guiObj, 'orbitYaw', 0, 1);
+
+  setCustomConf(guiObj);
+}
+
+function createGuiObject(generator) {
+  return {
+    seed: globalThis.seed,
+    starSize: 20,
+    celBodyRotationsPerUnit: 0.1,
+    minTilt: 0.0,
+    maxTilt: Math.PI / 4,
+    planetMinSize: 1,
+    planetMaxSize: 6,
+    minRevolutionsPerUnit: 1,
+    maxRevolutionsPerUnit: 2,
+    minMoonAmount: 0,
+    maxMoonAmount: 4,
+    minMoonRevolutionsPerUnit: 1,
+    maxMoonRevolutionsPerUnit: 2,
+    moonMinTilt: 0.0,
+    moonMaxTilt: Math.PI / 4,
+    visibleOrbits: true,
+    ellipticalOrbit: true,
+    minDistanceBetweenOrbits: 30,
+    minOrbitTiltX: - Math.PI / 20,
+    maxOrbitTiltX: Math.PI / 20,
+    minOrbitTiltZ: - Math.PI / 20,
+    maxOrbitTiltZ: Math.PI / 20,
+    minEllipseX: 0.9,
+    maxEllipseX: 1.1,
+    minEllipseZ: 0.9,
+    maxEllipseZ: 1.1,
+    orbitYaw: 0.5,
+    Generate: function () {
+      scene.remove(core.root);
+      setCustomConf(this);
+      core = generator.generate(this.seed);
+      scene.add(core.root);
+    }
+  };
+}
+
+function setCustomConf(guiObject) {
   var customConf = new Map();
-  customConf.set("starSize", 20);
-  customConf.set("planetMinSize", 1);
-  customConf.set("planetMaxSize", 6);
-  customConf.set("minRevolutionsPerUnit", 1);
-  customConf.set("maxRevolutionsPerUnit", 2);
-  customConf.set("minMoonAmount", 0);
-  customConf.set("maxMoonAmount", 4);
-  customConf.set("minMoonRevolutionsPerUnit", 1.0);
-  customConf.set("maxMoonRevolutionsPerUnit", 2.0);
-  customConf.set("minOrbitTiltX", - Math.PI / 20); // 9 degrees
-  customConf.set("maxOrbitTiltX", Math.PI / 20);
-  customConf.set("minOrbitTiltZ", - Math.PI / 20);
-  customConf.set("maxOrbitTiltZ", Math.PI / 20);
-  customConf.set("ellipticalOrbit", true);
-  customConf.set("minEllipseX", 0.9);
-  customConf.set("maxEllipseX", 1.1);
-  customConf.set("minEllipseZ", 0.9);
-  customConf.set("maxEllipseZ", 1.1);
-  customConf.set("celBodyRotationsPerUnit", 0.1);
+  for( prop in guiObject ){
+    if(prop == 'seed' || prop == 'Generate') continue;
+    customConf.set(prop, guiObject[prop]);
+  }
   confMap = new Conf(customConf).confMap;
 }
 
